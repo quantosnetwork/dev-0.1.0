b0e48d153bc709952867dd64112ab862169b8fa9
Quantos Blockchain & Network <97577935+quantosnetwork@users.noreply.github.com>
2022-04-03 23:34:49 -0400

    Update fees.go

--- core/tx/fees/fees.go
+++ core/tx/fees/fees.go
@@ -3,6 +3,6 @@ package fees
 // CalculateFees fee formula: (value * 100) / (numbytes * deflation) / 1024 / 2
 // CalculateFees per transaction implements fee formula
 func CalculateFees(value int64, numbytes int64, deflation float64) float64 {
-	f := float64(value*100) / (float64(numbytes) * deflation) / 1024 / 2
+	f := (float64(value) * 100) / (float64(numbytes) * deflation) / 1024 / 2
 	return f
 }
